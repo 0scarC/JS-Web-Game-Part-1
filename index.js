4b2ffaeb667bf3d7e3b4d21b858b0c46461d0eff
Admin <admin@Admins-MacBook-Pro-2.local>
2022-04-18 19:07:58 -0500

    Main portion of assignemtn complete

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage(location, left, bottom) {
+    let asset = document.createElement('img')
+    asset.src = location
+    asset.style.position = 'fixed'
+    asset.style.left = left + 'px'
+    asset.style.bottom = bottom + 'px'
+    document.body.append(asset)    
+}
+
+function newItem(location, left, bottom) {
+    let asset = document.createElement('img')
+    asset.src = location
+    asset.style.position = 'fixed'
+    asset.style.left = left + 'px'
+    asset.style.bottom = bottom + 'px'
+    document.body.append(asset)
+
+    asset.addEventListener('dblclick', function() {
+        asset.remove()
+    })
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
